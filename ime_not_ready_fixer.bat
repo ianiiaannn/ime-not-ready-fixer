04fd125c55a2c1b1b548a54d5b8fc695e3ef1284
ianiiaannn <50264353+ianiiaannn@users.noreply.github.com>
2020-11-10 22:23:20 +0800

    Add files via upload

---
+++ ime_not_ready_fixer.bat
@@ -0,0 +1,10 @@
+
+@echo off
+cd C:\Windows\SoftwareDistribution
+echo start delete files that stuck...
+echo y|DEL /F/S/Q *.*
+echo ---------------------------------------
+set /p input="Which ime would you like to install(zh-TW,en_US...):"
+DISM.exe /Online /Add-Capability  /CapabilityName:Language.Basic~~~%input%~0.0.1.0
+echo done!
+pause
