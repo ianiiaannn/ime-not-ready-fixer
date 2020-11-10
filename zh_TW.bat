89558c839513f05f6274926e2d84fd0525db19f9
ianiiaannn <50264353+ianiiaannn@users.noreply.github.com>
2020-11-10 22:23:39 +0800

    Delete zh_TW.bat

--- zh_TW.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd C:\Windows\SoftwareDistribution
-echo start delete file that stuck...
-echo y|DEL /F/S/Q *.*
-echo start download ime file...
-DISM.exe /Online /Add-Capability  /CapabilityName:Language.Basic~~~zh-TW~0.0.1.0
-echo done!
-pause
